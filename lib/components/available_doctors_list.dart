dcd4ce745426ca5114bb8cfeffd90d6ea6b3f176
alexalmeida52 <jhosealex@gmail.com>
2022-05-09 22:44:16 -0300

    search doctor by name

--- lib/components/available_doctors_list.dart
+++ lib/components/available_doctors_list.dart
@@ -6,86 +6,84 @@ import '../models/doctor.dart';
 import '../utils/app_routes.dart';
 
 class AvailableDoctorsList extends StatefulWidget {
+  List<Doctor> _filteredListDoctors;
+
+  AvailableDoctorsList(this._filteredListDoctors);
+
   @override
   State<AvailableDoctorsList> createState() => _AvailableDoctorsListState();
 }
 
 class _AvailableDoctorsListState extends State<AvailableDoctorsList> {
-  List<Doctor> doctorsList = doctorsListMocked;
-
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: doctorsList.isEmpty
-            ? Text('Nenhuma tarefa cadastrada')
+        child: widget._filteredListDoctors.isEmpty
+            ? Text('Nenhuma profissional encontrado')
             : Expanded(
-              child: ListView.builder(
+                child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: doctorsList.length,
+                  itemCount: widget._filteredListDoctors.length,
                   itemBuilder: (context, index) {
-                    final doctor = doctorsList[index];
+                    final doctor = widget._filteredListDoctors[index];
                     return Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
                       child: InkWell(
                         onTap: () {
-                          Navigator.of(context).pushNamed(
-                            AppRoutes.CREATE_SCHEDULE
-                          );
+                          Navigator.of(context)
+                              .pushNamed(AppRoutes.CREATE_SCHEDULE);
                         },
                         child: Card(
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  width: 80.0,
-                                  height: 80.0,
-                                  decoration: new BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      image: new DecorationImage(
-                                          fit: BoxFit.fill,
-                                          image: new AssetImage(
-                                              doctor.avatarUrl != null 
-                                              ? 'assets/${doctor.avatarUrl!}' 
-                                              : 'assets/user_default.png'
-                                          )
-                                      )
-                                  )),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      doctor.name, 
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: Color.fromRGBO(28, 45, 62, 1)
-                                      )
-                                    ),
-                                    Text(
-                                      doctor.speciality,
-                                      style: TextStyle(
-                                        color: Color.fromRGBO(28, 45, 62, 1)
-                                      )
-                                    ),
-                                    Text(
-                                      doctor.address,
-                                      style: TextStyle(
-                                        color: Color.fromRGBO(28, 45, 62, 1)
-                                      )
-                                    )
-                                  ],
-                                ),
-                                Icon(Icons.bookmark_outline_outlined, size: 40, color: Color.fromRGBO(28, 45, 62, 1),),
-                              ]
-                            ),
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                      width: 80.0,
+                                      height: 80.0,
+                                      decoration: new BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          image: new DecorationImage(
+                                              fit: BoxFit.fill,
+                                              image: new AssetImage(doctor
+                                                          .avatarUrl !=
+                                                      null
+                                                  ? 'assets/${doctor.avatarUrl!}'
+                                                  : 'assets/user_default.png')))),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(doctor.name,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Color.fromRGBO(
+                                                  28, 45, 62, 1))),
+                                      Text(doctor.speciality,
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  28, 45, 62, 1))),
+                                      Text(doctor.address,
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  28, 45, 62, 1)))
+                                    ],
+                                  ),
+                                  Icon(
+                                    Icons.bookmark_outline_outlined,
+                                    size: 40,
+                                    color: Color.fromRGBO(28, 45, 62, 1),
+                                  ),
+                                ]),
                           ),
                         ),
                       ),
                     );
                   },
                 ),
-            ));
+              ));
   }
 }
--- lib/screens/available_doctors.dart
+++ lib/screens/available_doctors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imd0509_projeto/components/available_doctors_list.dart';
 
+import '../models/doctor.dart';
 import '../utils/app_routes.dart';
+import '../utils/mock/doctorsList.dart';
 
 class AvailableDoctors extends StatefulWidget {
   @override
@@ -9,6 +11,28 @@ class AvailableDoctors extends StatefulWidget {
 }
 
 class _AvailableDoctorsState extends State<AvailableDoctors> {
+  List<Doctor> _doctorsList = doctorsListMocked;
+  List<Doctor> _filteredListDoctors = [];
+  String _searchText = '';
+  final _searchDoctorController = TextEditingController();
+
+  initState() {
+    // at the beginning, all users are shown
+    _filteredListDoctors = _doctorsList;
+    super.initState();
+  }
+
+  _filterDoctors(String value) {
+    print(value);
+    setState(() {
+      _filteredListDoctors = _doctorsList
+          .where((doctor) =>
+              doctor.name.toLowerCase().contains(value.toLowerCase()) ||
+              value == '')
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,32 +43,41 @@ class _AvailableDoctorsState extends State<AvailableDoctors> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
+              controller: _searchDoctorController,
+              onChanged: _filterDoctors,
               decoration: InputDecoration(
+                suffixIcon: InkWell(
+                    onTap: () {
+                      _searchDoctorController.text = '';
+                    },
+                    child: Icon(Icons.close)),
+                prefixIcon: Icon(Icons.search),
                 fillColor: Colors.white,
                 filled: true,
                 contentPadding: EdgeInsets.all(18.0),
-                label: Text('Nome do profissional', style: TextStyle(color: Colors.black.withOpacity(0.3), fontWeight: FontWeight.bold),),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 2.0
-                  ),
-                  borderRadius: BorderRadius.circular(0)
+                label: Text(
+                  'Nome do profissional',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.3),
+                      fontWeight: FontWeight.bold),
                 ),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2.0),
+                    borderRadius: BorderRadius.circular(0)),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            Text('Médicos disponíveis', 
-              style: TextStyle(
-                color: Color.fromRGBO(28, 45, 62, 1),
-                fontSize: 26
-              ),
+            Text(
+              'Médicos disponíveis',
+              style:
+                  TextStyle(color: Color.fromRGBO(28, 45, 62, 1), fontSize: 26),
             ),
             SizedBox(
               height: 20,
             ),
-            AvailableDoctorsList()
+            AvailableDoctorsList(_filteredListDoctors)
           ],
         ),
       ),

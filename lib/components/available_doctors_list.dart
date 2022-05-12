667bb9db3e9076164c08c094296ca82c329c5241
alexalmeida52 <jhosealex@gmail.com>
2022-05-11 21:54:01 -0300

    Bugfix click on list item

--- lib/components/available_doctors_list.dart
+++ lib/components/available_doctors_list.dart
@@ -23,7 +23,7 @@ class _AvailableDoctorsListState extends State<AvailableDoctorsList> {
   Widget build(BuildContext context) {
     return Container(
         child: widget._filteredListDoctors.isEmpty
-            ? Text('Nenhuma profissional encontrado')
+            ? Text('Nenhum profissional encontrado')
             : Expanded(
                 child: ListView.builder(
                   shrinkWrap: true,
@@ -32,83 +32,77 @@ class _AvailableDoctorsListState extends State<AvailableDoctorsList> {
                     final doctor = widget._filteredListDoctors[index];
                     return Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context)
-                              .pushNamed(AppRoutes.CREATE_SCHEDULE);
-                        },
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                      width: 80.0,
-                                      height: 80.0,
-                                      decoration: new BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: new AssetImage(doctor
-                                                          .avatarUrl !=
-                                                      null
-                                                  ? 'assets/${doctor.avatarUrl!}'
-                                                  : 'assets/user_default.png')))),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(doctor.name,
-                                          style: TextStyle(
-                                              fontSize: 18,
-                                              color: Color.fromRGBO(
-                                                  28, 45, 62, 1))),
-                                      Text(doctor.speciality,
-                                          style: TextStyle(
-                                              color: Color.fromRGBO(
-                                                  28, 45, 62, 1))),
-                                      Text(doctor.address,
-                                          style: TextStyle(
-                                              color: Color.fromRGBO(
-                                                  28, 45, 62, 1))),
-                                      Row(
-                                        children: [
-                                          ElevatedButton(
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                    width: 80.0,
+                                    height: 80.0,
+                                    decoration: new BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: new DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: new AssetImage(doctor
+                                                        .avatarUrl !=
+                                                    null
+                                                ? 'assets/${doctor.avatarUrl!}'
+                                                : 'assets/user_default.png')))),
+                                Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  children: [
+                                    Text(doctor.name,
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            color: Color.fromRGBO(
+                                                28, 45, 62, 1))),
+                                    Text(doctor.speciality,
+                                        style: TextStyle(
+                                            color: Color.fromRGBO(
+                                                28, 45, 62, 1))),
+                                    Text(doctor.address,
+                                        style: TextStyle(
+                                            color: Color.fromRGBO(
+                                                28, 45, 62, 1))),
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pushNamed(
+                                                  '/profile_doctor',
+                                                  arguments: doctor);
+                                              //_selectedDoctor(doctor);
+                                            },
+                                            child: Text('Perfil')),
+                                        Container(
+                                          padding:
+                                              const EdgeInsets.only(left: 5),
+                                          width: 100,
+                                          child: ElevatedButton(
                                               onPressed: () {
-                                                Navigator.of(context).pushNamed(
-                                                    '/profile_doctor',
-                                                    arguments: doctor);
-                                                //_selectedDoctor(doctor);
+                                                Navigator.of(context)
+                                                    .pushNamed(
+                                                        '/create_schecule',
+                                                        arguments: doctor);
                                               },
-                                              child: Text('Perfil')),
-                                          Container(
-                                            padding:
-                                                const EdgeInsets.only(left: 5),
-                                            width: 100,
-                                            child: ElevatedButton(
-                                                onPressed: () {
-                                                  Navigator.of(context)
-                                                      .pushNamed(
-                                                          '/create_schecule',
-                                                          arguments: doctor);
-                                                },
-                                                child:
-                                                    Text('Agendar consulta')),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                  Icon(
-                                    Icons.bookmark_outline_outlined,
-                                    size: 40,
-                                    color: Color.fromRGBO(28, 45, 62, 1),
-                                  ),
-                                ]),
-                          ),
+                                              child:
+                                                  Text('Agendar consulta')),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Icon(
+                                  Icons.bookmark_outline_outlined,
+                                  size: 40,
+                                  color: Color.fromRGBO(28, 45, 62, 1),
+                                ),
+                              ]),
                         ),
                       ),
                     );

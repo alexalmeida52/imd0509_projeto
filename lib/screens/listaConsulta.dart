bd6178e0f90acac1641e5a08ab9f086d08aa47f0
alexalmeida52 <jhosealex@gmail.com>
2022-05-11 22:36:01 -0300

    show tab selected

--- lib/screens/listaConsulta.dart
+++ lib/screens/listaConsulta.dart
@@ -8,34 +8,37 @@ import '../utils/mock/consultaList.dart';
 class ConsultaMedica extends StatefulWidget {
   @override
   State<ConsultaMedica> createState() => _ConsultaMedicaState();
-  
+
 }
 
 class _ConsultaMedicaState extends State<ConsultaMedica> {
   List<Consulta> _consutList = consultaListMocked;
   List<Consulta> _filterListConsulta = [];
+  int tabSelected = 2;
 
   initState() {
     // at the beginning, all users are shown
-    _filterListConsulta = _consutList;
+    _filterConsultaFuturas(); 
     super.initState();
   }
 
   _filterConsulta() {
     //print(value);
     setState(() {
+      tabSelected = 1;
       _filterListConsulta = _consutList
           .where((consulta) =>
-               consulta.data.compareTo(DateTime.now()) < 0)
+              consulta.data.compareTo(DateTime.now()) < 0)
           .toList();
     });
   }
 
   _filterConsultaFuturas() {
     setState(() {
+      tabSelected = 2;
       _filterListConsulta = _consutList
           .where((consulta) =>
-               consulta.data.compareTo(DateTime.now()) > 0)
+              consulta.data.compareTo(DateTime.now()) > 0)
           .toList();
     });
   }
@@ -50,42 +53,50 @@ class _ConsultaMedicaState extends State<ConsultaMedica> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        alignment: Alignment.bottomCenter,
-                        padding: const EdgeInsets.all(8.0),
-                        //color: Colors.red,
-                        child: ElevatedButton(
-                          onPressed: _filterConsulta,
-                          child: Text('PASSADAS'),
-                          style: ElevatedButton.styleFrom(
-                              primary:  Colors.white,
-                              onPrimary:  Colors.greenAccent,
-                              side: BorderSide(width: 3.0, color: Colors.greenAccent)),
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment.bottomCenter,
-                        padding: const EdgeInsets.all(8.0),
-                        //color: Colors.blue,
-                        child: ElevatedButton(
-                          onPressed: _filterConsultaFuturas,
-                          child: Text('FUTURAS'),
-                          style: ElevatedButton.styleFrom(
-                               primary:  Colors.white,
-                               onPrimary: Colors.greenAccent,
-                              side: BorderSide(width: 3.0, color: Colors.greenAccent)),
-                        ),
-                      )
-                    ],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    padding: const EdgeInsets.all(8.0),
+                    //color: Colors.red,
+                    child: ElevatedButton(
+                      onPressed: _filterConsulta,
+                      child: Text('PASSADAS'),
+                      style: ElevatedButton.styleFrom(
+                          primary:  Colors.white,
+                          onPrimary:  Colors.greenAccent,
+                          side: tabSelected == 1
+                              ? BorderSide(
+                                  width: 3.0, color: Colors.greenAccent)
+                              : BorderSide(
+                                  color: Colors.black.withOpacity(0.1))),
+                    ),
                   ),
-                ),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    padding: const EdgeInsets.all(8.0),
+                    //color: Colors.blue,
+                    child: ElevatedButton(
+                      onPressed: _filterConsultaFuturas,
+                      child: Text('FUTURAS'),
+                      style: ElevatedButton.styleFrom(
+                          primary:  Colors.white,
+                          onPrimary: Colors.greenAccent,
+                          side: tabSelected == 2
+                              ? BorderSide(
+                                  width: 3.0, color: Colors.greenAccent)
+                              : BorderSide(
+                                  color: Colors.black.withOpacity(0.1))),
+                    ),
+                  )
+                ],
+              ),
+            ),
             SizedBox(
               height: 20,
             ),
-            // Text('Médicos disponíveis', 
+            // Text('Médicos disponíveis',
             //   style: TextStyle(
             //     color: Color.fromRGBO(28, 45, 62, 1),
             //     fontSize: 26
